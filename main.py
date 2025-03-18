daeec6b421b4ccd493c0f7b6ed514435a1dc0f2c
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-17 21:26:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if selected_page == webpages[1]:
     
     # Alternative option: Provide a clickable link
     st.subheader("Alternative Link")
-    st.markdown(f'[Click here to play Getaway Shooter]({game_urls["getaway_shootout"]})', unsafe_allow_html=True)
+    st.markdown(f'[Click here to play Getaway Shootout]({game_urls["getaway_shootout"]})', unsafe_allow_html=True)
     
     # Additional app features
-    st.text("Enjoy playing Getaway Shooter!")
+    st.text("Enjoy playing Getaway Shootout!")
