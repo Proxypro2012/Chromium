4943d9eaea9307d19eefd0581eb0fca6da54d063
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-17 21:01:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ st.title("Play Getaway Shooter Game")
 # Game URL to be embedded
 game_url = "https://selenite.cc/semag/getawayshooter/index.html"
 
-# Embed the game in an iframe
-components.iframe(game_url, width=800, height=600)
+# Try embedding the game in an iframe
+st.subheader("Embedded Game (May not work due to security restrictions)")
+try:
+    components.iframe(game_url, width=800, height=600)
+except:
+    st.error("Embedding failed. Use the link below to play the game.")
+
+# Alternative option: Provide a clickable link
+st.subheader("Alternative Link")
+st.markdown(f'[Click here to play Getaway Shooter]({game_url})', unsafe_allow_html=True)
 
 # Additional app features
 st.text("Enjoy playing Getaway Shooter!")
