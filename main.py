0802ba88a323d5e5be2b43d3fc03265b0997c72e
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-17 21:48:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ game_urls = {
 
 
 webpages = ["Home", "Games", "PictureOfTheDay"]
-selected_page = st.sidebar.radio("Navigation", webpages)
+st.sidebar.header("Navigation")
+selected_page = st.sidebar.radio("", webpages)
+st.sidebar.image("logo.png")
 
 
 
