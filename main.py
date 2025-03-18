7062a470e5abef82dea2b5f6d5a30b3f82c1fa6e
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-17 21:12:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import streamlit.components.v1 as components
 st.title("Chromium")
 st.write("Versatile web proxy. For my guys at school.")
 
-# Game URL to be embedded
+# Game URLs dictionary
 game_urls = {
-             getaway_shootout: "https://selenite.cc/semag/getawayshooter/index.html"
+    "getaway_shootout": "https://selenite.cc/semag/getawayshooter/index.html"
 }
 
 # Try embedding the game in an iframe
 st.subheader("Embedded Game (May not work due to security restrictions)")
 try:
-    components.iframe(game_urls['getaway_shootout'], width=1500, height=700)
+    components.iframe(game_urls["getaway_shootout"], width=1500, height=700)
 except:
-    st.error("Embedding failed. Use the link below to play the game.")
+    st.error("Sorry guys, looks like this game just got blocked :(")
 
 # Alternative option: Provide a clickable link
 st.subheader("Alternative Link")
-st.markdown(f'[Click here to play Getaway Shooter]({game_url})', unsafe_allow_html=True)
+st.markdown(f'[Click here to play Getaway Shooter]({game_urls["getaway_shootout"]})', unsafe_allow_html=True)
 
 # Additional app features
 st.text("Enjoy playing Getaway Shooter!")
