cf2a4fc07873923ac1ce0796aba537d7d2149949
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-17 21:19:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,18 +19,18 @@ st.write("Versatile web proxy. For my guys at school.")
 
 
 
-
-
-
-st.subheader("Getaway Shootout!")
-try:
-    components.iframe(game_urls["getaway_shootout"], width=1500, height=700)
-except:
-    st.error("Sorry guys, looks like this game just got blocked :(")
-
-# Alternative option: Provide a clickable link
-st.subheader("Alternative Link")
-st.markdown(f'[Click here to play Getaway Shooter]({game_urls["getaway_shootout"]})', unsafe_allow_html=True)
-
-# Additional app features
-st.text("Enjoy playing Getaway Shooter!")
+    
+    
+if selected_page == webpages[1]:
+    st.subheader("Getaway Shootout!")
+    try:
+        components.iframe(game_urls["getaway_shootout"], width=1500, height=700)
+    except:
+        st.error("Sorry guys, looks like this game just got blocked :(")
+    
+    # Alternative option: Provide a clickable link
+    st.subheader("Alternative Link")
+    st.markdown(f'[Click here to play Getaway Shooter]({game_urls["getaway_shootout"]})', unsafe_allow_html=True)
+    
+    # Additional app features
+    st.text("Enjoy playing Getaway Shooter!")
