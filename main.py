35156e64d8059ca76be0c13a2d70a16f57827b43
Proxypro2012 <kabir.tiwari@outlook.com>
2025-03-17 21:01:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,3 @@ import streamlit.components.v1 as components
 st.title("Play Getaway Shooter Game")
 
 # Game URL to be embedded
-game_url = "https://selenite.cc/semag/getawayshooter/index.html"
-
-# Try embedding the game in an iframe
-st.subheader("Embedded Game (May not work due to security restrictions)")
-try:
-    components.iframe(game_url, width=800, height=600)
-except:
-    st.error("Embedding failed. Use the link below to play the game.")
-
-# Alternative option: Provide a clickable link
-st.subheader("Alternative Link")
-st.markdown(f'[Click here to play Getaway Shooter]({game_url})', unsafe_allow_html=True)
-
-# Additional app features
-st.text("Enjoy playing Getaway Shooter!")
